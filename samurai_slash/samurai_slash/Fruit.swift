4d45de1d151f8d30deb32cd67c0b00f506f16230
Jessica Hernandez <cf17jessica.hernandez@iesjoandaustria.org>
2019-05-11 17:46:59 +0200

    Fruit elements and bombs modified to have specific score

--- samurai_slash/samurai_slash/Fruit.swift
+++ samurai_slash/samurai_slash/Fruit.swift
@@ -8,10 +8,38 @@
 
 import SpriteKit
 
+class ScreenElements {
+    var emoji =  String()
+    var name =  String()
+    var score =  Int()
+    
+    init(_ emoji: String, _ name: String, _ score: Int) {
+        self.emoji = emoji
+        self.name = name
+        self.score = score
+    }
+    
+}
+
 class Fruit: SKNode {
     
-    let fruitEmojis = ["🍏","🍉","🍌","🍊","🍓", "🍇", "🍍", "🍒", "🍐"]
-    let bombEmoji = ["💣", "🧨"]
+    let listOfElements = [
+        ScreenElements("🍏","manzana", 6),
+        ScreenElements("🍉","sandia", 5),
+        ScreenElements("🍌","platano", 5),
+        ScreenElements("🍊","naranja", 4),
+        ScreenElements("🍓","fresa", 4),
+        ]
+    
+    let listOfBombs = [
+        ScreenElements("💣","bomb", -10),
+        ScreenElements("🧨","tnt", -20)
+    ]
+    
+    //let fruitEmojis = ["🍏","🍉","🍌","🍊","🍓", "🍇", "🍍", "🍒", "🍐"]
+    //let nameFruit = ["manzana","sandia","platano","naranja","fresa", "uva", "piña", "cereza", "pera"]
+    //let bombEmoji = ["💣", "🧨"]
+    //let bombName = ["bomb","tnt"]
     
     override init(){
         super.init()
@@ -19,14 +47,15 @@ class Fruit: SKNode {
         var emoji = ""
         
         if randomCGFfloat(0, 1) < 0.9 {
-            name = "fruit"
-            let n = Int(arc4random_uniform(UInt32(fruitEmojis.count)))
-            emoji = fruitEmojis[n]
+            let n = Int(arc4random_uniform(UInt32(listOfElements.count)))
+            name = listOfElements[n].name
+            emoji = listOfElements[n].emoji
             
         } else {
-            name = "bomb"
-            let n = Int(arc4random_uniform(UInt32(bombEmoji.count)))
-            emoji = bombEmoji[n]
+            
+            let n = Int(arc4random_uniform(UInt32(listOfBombs.count)))
+            name = listOfBombs[n].name
+            emoji = listOfBombs[n].emoji
         }
         
         let label = SKLabelNode(text: emoji)
