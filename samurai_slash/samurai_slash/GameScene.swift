bd26c136bfac8e27bc85e095f52cdbaf3ca9669f
Jessica Hernandez <cf17jessica.hernandez@iesjoandaustria.org>
2019-05-11 15:27:03 +0200

    Init project

---
+++ samurai_slash/samurai_slash/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  samurai_slash
+//
+//  Created by Jessica Hernandez on 11/05/2019.
+//  Copyright © 2019 Jessica Hernandez. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        <#code#>
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+    
+    
+}
